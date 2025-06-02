c6056fb46f25f3dcd7c890e6ffdcde7dcb9d3f8a
Nolan Mastrodicasa <88843495+NolanMastrodicasa@users.noreply.github.com>
2025-06-02 00:50:35 -0700

    Update DiceGame.java

--- DiceGame.java
+++ DiceGame.java
@@ -1,14 +1,40 @@
-public abstract class DiceGame {
-    private final int playerMoney;
+/**
+ * base class for all dicegames, initializes playerMoney, creates getter and setter methods.
+ */
+public abstract class DiceGame extends GameLogger {
+    private int playerMoney;
 
-    public DiceGame(int startingMoney){
+    /**
+     * constructor to create a new dicegame with players money
+     *
+     * @param startingMoney the amount of money a player starts with.
+     */
+    public DiceGame(int startingMoney) {
         this.playerMoney = startingMoney;
     }
 
-    public int getPlayerMoney(){
+    /**
+     * returns players current balance
+     *
+     * @return int the players money
+     */
+    public int getPlayerMoney() {
         return playerMoney;
     }
 
-    public abstract void playRound(int betAmount);
+    /**
+     * adjusts the players money by specific amount.
+     * @param money int the amount to adjust by
+     */
+    public void adjustPlayerMoney(int money) {
+        playerMoney += money;
+    }
 
+    /**
+     * method to play a round of specified dicegame
+     * implemented by both StandardDiceGame and MultiDiceGame
+     *
+     * @param betAmount int the amount being bet in the current round.
+     */
+    public abstract void playRound(int betAmount);
 }
